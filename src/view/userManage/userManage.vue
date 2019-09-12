8c7207cfdf07a8b1c4b0e5549e6cb614d5d9b908
cmb007 <2351580924@qq.com>
2019-09-12 16:59:03 +0800

    完成用户管理界面UI

--- src/view/userManage/userManage.vue
+++ src/view/userManage/userManage.vue
@@ -1,75 +1,173 @@
-<style lang="less">
-    @import "./common.less";
-</style>
 <template>
   <div>
-    <Card title="导出EXCEL">
-      <Row>
-        <Button icon="md-download" :loading="exportLoading" @click="exportExcel">导出文件</Button>
-      </Row>
-    </Card>
-    <Row class="margin-top-10">
-      <Table :columns="tableTitle" :data="tableData"></Table>
-    </Row>
+    <div class="head">
+      搜索用户：<Input placeholder="请输入用户名" style="width: auto" />
+      <Button style="margin-left:8px;" type="primary">查询</Button>
+    </div>
+    <Table size="default" border :columns="columns" :data="list"></Table>
   </div>
 </template>
 <script>
-import excel from '@/libs/excel'
 export default {
-  name: 'export-excel',
+  name: 'userManage',
   data () {
     return {
-      exportLoading: false,
-      tableTitle: [
+      columns: [
         {
-          title: '一级分类',
-          key: 'category1'
+          title: '编号',
+          key: 'userId',
+          width: 100,
+          align: 'center'
         },
         {
-          title: '二级分类',
-          key: 'category2'
+          title: '用户名',
+          key: 'userName',
+          align: 'center',
+          width: 150
         },
         {
-          title: '三级分类',
-          key: 'category3'
+          title: '姓名',
+          key: 'name',
+          align: 'center',
+          width: 120
+        },
+        {
+          title: '性别',
+          key: 'gender',
+          align: 'center',
+          width: 100
+        },
+        {
+          title: '联系电话',
+          key: 'telephone',
+          align: 'center',
+          width: 200
+        },
+        {
+          title: '邮箱地址',
+          key: 'email',
+          align: 'center',
+          width: 200
+        },
+        {
+          title: '备注',
+          key: 'remark',
+          align: 'center'
+        },
+        {
+          title: '操作',
+          key: 'action',
+          fixed: 'right',
+          align: 'center',
+          width: 120,
+          render: (h, params) => {
+            return h('div', [
+              h('Button', {
+                props: {
+                  type: 'default',
+                  size: 'large'
+                }
+              }, '删除用户')
+            ])
+          }
         }
       ],
-      tableData: [
+      list: [
         {
-          category1: 1,
-          category2: 2,
-          category3: 3
+          userId: 1,
+          userName: 'bobo',
+          name: '蔡敏波',
+          gender: '男',
+          telephone: '13750416301',
+          email: '1535151019@qq.com',
+          remark: ''
         },
         {
-          category1: 4,
-          category2: 5,
-          category3: 6
+          userId: 1,
+          userName: 'bobo',
+          name: '蔡敏波',
+          gender: '男',
+          telephone: '13750416301',
+          email: '1535151019@qq.com',
+          remark: ''
         },
         {
-          category1: 7,
-          category2: 8,
-          category3: 9
+          userId: 1,
+          userName: 'bobo',
+          name: '蔡敏波',
+          gender: '男',
+          telephone: '13750416301',
+          email: '1535151019@qq.com',
+          remark: ''
+        },
+        {
+          userId: 1,
+          userName: 'bobo',
+          name: '蔡敏波',
+          gender: '男',
+          telephone: '13750416301',
+          email: '1535151019@qq.com',
+          remark: ''
+        },
+        {
+          userId: 1,
+          userName: 'bobo',
+          name: '蔡敏波',
+          gender: '男',
+          telephone: '13750416301',
+          email: '1535151019@qq.com',
+          remark: ''
+        },
+        {
+          userId: 1,
+          userName: 'bobo',
+          name: '蔡敏波',
+          gender: '男',
+          telephone: '13750416301',
+          email: '1535151019@qq.com',
+          remark: ''
+        },
+        {
+          userId: 1,
+          userName: 'bobo',
+          name: '蔡敏波',
+          gender: '男',
+          telephone: '13750416301',
+          email: '1535151019@qq.com',
+          remark: ''
+        },
+        {
+          userId: 1,
+          userName: 'bobo',
+          name: '蔡敏波',
+          gender: '男',
+          telephone: '13750416301',
+          email: '1535151019@qq.com',
+          remark: ''
+        },
+        {
+          userId: 1,
+          userName: 'bobo',
+          name: '蔡敏波',
+          gender: '男',
+          telephone: '13750416301',
+          email: '1535151019@qq.com',
+          remark: ''
+        },
+        {
+          userId: 1,
+          userName: 'bobo',
+          name: '蔡敏波',
+          gender: '男',
+          telephone: '13750416301',
+          email: '1535151019@qq.com',
+          remark: ''
         }
       ]
     }
   },
   methods: {
-    exportExcel () {
-      if (this.tableData.length) {
-        this.exportLoading = true
-        const params = {
-          title: ['一级分类', '二级分类', '三级分类'],
-          key: ['category1', 'category2', 'category3'],
-          data: this.tableData,
-          autoWidth: true,
-          filename: '分类列表'
-        }
-        excel.export_array_to_excel(params)
-        this.exportLoading = false
-      } else {
-        this.$Message.info('表格数据不能为空！')
-      }
-    }
+
   },
   created () {
 
@@ -79,3 +177,16 @@ export default {
   }
 }
 </script>
+<style lang="less">
+.ivu-table th {
+  font-size: 13px;
+}
+.ivu-table td {
+  font-size: 13px;
+}
+.head {
+  margin-bottom: 10px;
+  text-align: right;
+  font-size: 13px;
+}
+</style>
